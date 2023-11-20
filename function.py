5019bbe5ce88e62456e1a2daa00556e6bbe60a77
Melina <melina.cherchali@epfl.ch>
2023-11-20 19:24:40 +0100

    updates

--- function.py
+++ function.py
@@ -3,27 +3,32 @@ import pandas as pd
 import os 
 from sklearn.preprocessing import StandardScaler
 
+
 def read_file(file_path) :
     df = pd.read_csv(os.path.abspath(file_path), header=0)
-    return df 
+    df = pd.DataFrame(df.iloc[:,5:])
+    return df
 
-def clean_data(df_list):
-    for df in df_list :
-        #drop nan values
-        if (df.isna().any().any()):
-            df.dropna(inplace = True)
-        #removes constant predictors
-        df = df.loc[:, np.std(df, axis=0) != 0]
-        #removes perfectly correlated predictors 
-        correlation = np.array(df.corr().values) #compute correlation
-        correlation = np.triu(correlation, k=0) # remove values in double
-        np.fill_diagonal(correlation,0) # remove "self column" correlation
-        df = df.drop(df.columns[np.where(correlation==1)[1]], axis=1) #remove one of the column with exact correlation
+def clean_data(df):
+    print(len(df.columns))
+    #drop nan values
+    if (df.isna().any().any()):
+        df.dropna(inplace = True)
+    #removes constant predictors  
+    df = df.loc[:, np.std(df, axis=0) != 0]
+    #removes perfectly correlated predictors 
+    correlation = np.array(df.corr().values) #compute correlation
+    correlation = np.triu(correlation, k=0) # remove values in double
+    np.fill_diagonal(correlation,0) # remove "self column" correlation
+    df = df.drop(df.columns[np.where(correlation==1)[1]], axis=1) #remove one of the column with exact correlation
+    print(len(df.columns))
+    return df
 
 #The transformed data has now mean 0 and standard deviation 1 in every column.
 def standardize(df) :
     scaler = StandardScaler()
     df = pd.DataFrame(scaler.fit_transform(df), columns = [df.columns])
+    return df
     
     
 
