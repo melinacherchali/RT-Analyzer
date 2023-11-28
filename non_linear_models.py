8e5dddbb820e99065d4af26bc2119a05a145b85e
slne18 <solene.noize@epfl.ch>
2023-11-28 18:25:49 +0100

    m

--- non_linear_models.py
+++ non_linear_models.py
@@ -18,11 +18,18 @@ train_data_encoded, test_data_encoded = f.apply_one_hot_encoding(train_data, tes
 
 # Clean both train and test data using these columns
 train_data, test_data = f.clean_data(train_data_encoded, test_data_encoded)
+
+# Separate the features X and the target variable y
 train_subset = int(len(train_data) * 0.8)
 
+X = train_data.drop(['RT'], axis=1)
+y = train_data['RT']
+
 # Separate train and test data
-X, y = train_data.drop('RT', axis=1), train_data['RT']
-X_train, X_test, y_train, y_test = f.split_data(train_data)
+X_train = X.iloc[:train_subset, :]
+X_test = X.iloc[train_subset:, :]
+y_train = y.iloc[:train_subset]
+y_test = y.iloc[train_subset:]
 
 """ # Apply PCA
 variance_ratio = 0.989
@@ -36,43 +43,35 @@ y_test = y.iloc[train_subset:]
 
 # --------------------------------------------------- Random Forest --------------------------------------------------- 
 
-# Define the parameter grid
 param_grid = {
     'n_estimators': [100, 200, 300],
-    'max_features': ['sqrt', 'log2', .5],
+    'max_features': ['auto', 'sqrt', 'log2'],
     'max_depth': [None, 10, 20],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
     'bootstrap': [True, False]
 }
 
-# Model testing parameters
-submit = False
-file_path = 'random_forest_sub.csv'
-plot = True
-
-# Tune with Random Forest model
-# rf_model = f.tune_model(RandomForestRegressor(), train_data, param_grid, submit)
-
 rf_model = RandomForestRegressor(n_estimators=500)
-grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
-f.model_test(train_data,test_data,rf_model,submit,file_path,plot)
 
+grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
 
 # Train the model to submit (on test set)
 #rf_model.fit(X, y)
 
 # Train the model 
-# rf_model.fit(X_train,y_train)
+rf_model.fit(X_train,y_train)
 
 # Evaluate the model to submit (on test_data)
 #y_pred = pd.DataFrame(rf_model.predict(test_data))
 
 # Evaluate the model (on X_test)
-# y_pred = pd.DataFrame(rf_model.predict(X_test))
+y_pred = pd.DataFrame(rf_model.predict(X_test))
 
 # Test error 
-# f.test_error(y_test, y_pred, True)
+f.test_error(y_test, y_pred, True)
 
 # Make submission
 #f.make_submission(y_pred, "random_forest_sub.csv")
+
+
