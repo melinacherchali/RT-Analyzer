e4065ee9c9cd2b83cd0124e24e3c61ed0348c8cb
Melina <melina.cherchali@epfl.ch>
2023-11-29 15:54:17 +0100

    m

--- non_linear_models.py
+++ non_linear_models.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
 from xgboost import XGBRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import make_scorer
@@ -38,46 +39,63 @@ y_test = y.iloc[train_subset:]
 """
 # --------------------------------------------------- Gradient boosting ---------------------------------------------------
 
-param_grid = {
-    'colsample_bytree': np.linspace(0.5, 1, 5),
-    'subsample': np.linspace(0.5, 1, 5),
-    'max_depth': np.arange(2, 7, 1)
-}
+# param_grid = {
+#     'colsample_bytree': np.linspace(0.5, 1, 5),
+#     'subsample': np.linspace(0.5, 1, 5),
+#     'max_depth': np.arange(2, 7, 1)
+# }
 
-# Model testing parameters
-submit = True
-file_path = 'GB_sub.csv'
-plot = True
+# # Model testing parameters
+# submit = True
+# file_path = 'GB_sub.csv'
+# plot = True
 
-# Tune with Gradient boosting model
-#model_GB = f.tune_model(XGBRegressor(), train_data, param_grid, submit)
-model_GB = XGBRegressor()
-f.model_test(train_data,test_data,model_GB,submit,file_path,plot)
+# # Tune with Gradient boosting model
+# #model_GB = f.tune_model(XGBRegressor(), train_data, param_grid, submit)
+# model_GB = XGBRegressor()
+# f.model_test(train_data,test_data,model_GB,submit,file_path,plot)
 
 # --------------------------------------------------- Random Forest --------------------------------------------------- 
 
-""" # Define the parameter grid
-param_grid = {
-    'n_estimators': [100, 200, 300],
-    'max_features': ['sqrt', 'log2', .5],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'bootstrap': [True, False]
-}
+# Define the parameter grid
+# param_grid = {
+#     'n_estimators': [100, 200, 300],
+#     'max_features': ['sqrt', 'log2', .5],
+#     'max_depth': [None, 10, 20],
+#     'min_samples_split': [2, 5, 10],
+#     'min_samples_leaf': [1, 2, 4],
+#     'bootstrap': [True, False]
+# }
 
-# Model testing parameters
+# # Model testing parameters
 submit = False
 file_path = 'random_forest_sub.csv'
 plot = True
 
-# Tune with Random Forest model
+# # Tune with Random Forest model
 # rf_model = f.tune_model(RandomForestRegressor(), train_data, param_grid, submit)
+# # rf_model = RandomForestRegressor(n_estimators=500)
 
-rf_model = RandomForestRegressor(n_estimators=500)
-f.model_test(train_data,test_data,rf_model,submit,file_path,plot)
+# f.model_test(train_data,test_data,rf_model.best_estimator_,submit,file_path,plot)
 
- """
+param_dist = {
+    'n_estimators': [100, 200, 300, 400, 500],
+    'max_features': ['sqrt', 'log2', .5],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10, 15],
+    'min_samples_leaf': [1, 2, 4, 6],
+    'bootstrap': [True, False]
+}
+
+# Use RandomizedSearchCV instead of GridSearchCV
+rf_model = RandomizedSearchCV(RandomForestRegressor(), param_distributions=param_dist, n_iter=20, cv=5, n_jobs=-1)
+
+# Fit the model
+X = train_data.iloc[:train_subset,:]
+rf_model.fit(X.drop('RT', axis=1), X['RT'])
+
+# Get the best parameters
+f.model_test(train_data,test_data,rf_model.best_estimator_,submit,file_path,plot)
 
 
 
